e13c4fa10d3cacc6594482a9d3dbc7a9237366d0
CrepinAradjo228 <aradjocrepin@gmail.com>
2025-05-27 23:41:24 +0000

    Deuxième commit

--- SmartBiz_Manager/urls.py
+++ SmartBiz_Manager/urls.py
@@ -5,4 +5,5 @@ from SmartBiz_Manager import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('gestion_ventes/',views.ventes,name='ventes'),
 ]
--- SmartBiz_Manager/views.py
+++ SmartBiz_Manager/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
 def dashboard(request):
     return render(request, 'gestion_vente/dashboard_vente.html')
+
+def ventes(request):
+    return render(request, 'gestion_vente/index.html')
