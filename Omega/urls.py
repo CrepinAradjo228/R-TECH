c55d4dfdff07b89ee3fbb0f4e2657780a4f698c8
CrepinAradjo228 <aradjocrepin@gmail.com>
2025-05-29 13:28:11 +0000

    Gestion vente

--- Omega/urls.py
+++ Omega/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include('SmartBiz_Manager.urls')),
+    path('',include('SmartBiz_Manager.urls')),
 ]
 
--- SmartBiz_Manager/urls.py
+++ SmartBiz_Manager/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from SmartBiz_Manager import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('gestion_ventes/',views.ventes,name='ventes'),
+
+    
 ]
