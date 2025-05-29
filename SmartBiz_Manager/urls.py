bcd0c9bdbff8238a6ffeab6a79cd54bb906adf7c
CrepinAradjo228 <aradjocrepin@gmail.com>
2025-05-29 22:44:28 +0000

    Changement des urls

--- SmartBiz_Manager/urls.py
+++ SmartBiz_Manager/urls.py
@@ -8,19 +8,16 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     # Chemin pour la gestion des ventes - panier
-    path('vente/', views.index, name='index'),  # j'ai changé '' en 'vente/' pour éviter conflit avec home
-    path('vente/panier/', views.afficher_panier, name='afficher_panier'),
-    path('vente/panier/ajouter/<int:article_id>/', views.ajouter_au_panier, name='ajouter_au_panier'),
-    path('vente/panier/modifier/<int:article_id>/', views.modifier_quantite_panier, name='modifier_quantite_panier'),
-    path('vente/panier/supprimer/<int:article_id>/', views.supprimer_du_panier, name='supprimer_du_panier'),
-    path('vente/panier/valider/', views.valider_vente, name='valider_vente'),
+    path('ventes/', views.index, name='ventes'),  # j'ai changé '' en 'vente/' pour éviter conflit avec home
+    path('ventes/panier/', views.afficher_panier, name='afficher_panier'),
+    path('ventes/panier/ajouter/<int:article_id>/', views.ajouter_au_panier, name='ajouter_au_panier'),
+    path('ventes/panier/modifier/<int:article_id>/', views.modifier_quantite_panier, name='modifier_quantite_panier'),
+    path('ventes/panier/supprimer/<int:article_id>/', views.supprimer_du_panier, name='supprimer_du_panier'),
+    path('ventes/panier/valider/', views.valider_vente, name='valider_vente'),
 
         
 
 
-    # Gestion des ventes (une seule fois)
-
-    path('gestion_ventes/', views.ventes, name='ventes'),
 
     path('articles/', views.articles, name='articles'),
     path('ajout_article/', views.ajout_article, name='ajout_article'),
